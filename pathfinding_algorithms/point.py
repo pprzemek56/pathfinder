55f89d802eda5d3bbc1c1825b65b5b8c9a2c322d
pprzemek56 <przemkopis@wp.pl>
2023-02-16 00:02:56 +0100

    add representation od point

---
+++ pathfinding_algorithms/point.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        self._y = y
