bd79237a956671effe58542d9dbf086c95246265
pprzemek56 <przemkopis@wp.pl>
2023-04-02 20:47:58 +0200

    create board with placing the walls and remove it utility

--- templates/js/pathfinder.js
+++ templates/js/pathfinder.js
@@ -1,22 +1,112 @@
 const canvas = document.getElementById("game_canvas");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener('click', click_board);
+canvas.addEventListener('mousedown', move_mouse);
+canvas.addEventListener('mousemove', move_mouse);
 
 const HORIZONTAL_SQUARES = 53;
 const VERTICAL_SQUARES = 21;
-const BOARD_WIDTH = canvas.offsetWidth;
-const BOARD_HEIGHT = initHeight();
+const BOARD_WIDTH = 1272;
+const BOARD_HEIGHT = 504;
 
-function initHeight() {
-    let square_height = BOARD_WIDTH / 53;
-    canvas.style.height = square_height * 21 + "px";
-    return canvas.height;
+const a_square = 24;
+
+//0 = empty square
+//1 = wall square
+//2 = start square
+//3 = end square
+let board = initBoard();
+let prevSquare = null;
+
+function move_mouse(event) {
+    let x = event.clientX - canvas.offsetLeft;
+    let y = event.clientY - canvas.offsetTop;
+
+    if (x > 0 && x < 1272 && y > 0 && y < 504) {
+        x = Math.floor(x / 24)
+        y = Math.floor(y / 24)
+    }
+    if (event.buttons === 1) {
+        if (prevSquare !== null && (prevSquare.x !== x || prevSquare.y !== y)) {
+            if (board[prevSquare.y][prevSquare.x] === 0) {
+                drawRectangle(prevSquare.x, prevSquare.y, "white");
+            } else if (board[prevSquare.y][prevSquare.x] === 1) {
+                drawRectangle(prevSquare.x, prevSquare.y, "#0f3052");
+            }
+        }
+
+        if (board[y][x] === 0 && (prevSquare === null || prevSquare.x !== x || prevSquare.y !== y)) {
+            board[y][x] = 1;
+            drawRectangle(x, y, "#0f3052");
+        }
+        else if (board[y][x] === 1 && (prevSquare === null || prevSquare.x !== x || prevSquare.y !== y)) {
+            board[y][x] = 0;
+            drawRectangle(x, y, "white");
+        }
+        prevSquare = {x: x, y: y};
+    }
 }
 
+
+function click_board(event) {
+    let x = event.clientX - canvas.offsetLeft;
+    let y = event.clientY - canvas.offsetTop;
+
+    if (x > 0 && x < 1272 && y > 0 && y < 504) {
+        x = Math.floor(x / 24)
+        y = Math.floor(y / 24)
+    }
+
+    if (board[y][x] === 0){
+        board[y][x] = 1;
+        drawRectangle(x, y, "#0f3052");
+    }else if (board[y][x] === 1){
+        board[y][x] = 0;
+        drawRectangle(x, y, "white");
+    }
+}
+
+function drawRectangle(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * 24, y * 24, 24, 24);
+    ctx.strokeStyle = "#0f3052";
+    ctx.strokeRect(x * 24, y * 24, 24, 24);
+}
+
+
+function initBoard() {
+
+    let array = new Array(VERTICAL_SQUARES)
+    for (let i = 0; i < VERTICAL_SQUARES; i++){
+        array[i] = new Array(HORIZONTAL_SQUARES).fill(0);
+    }
+
+    array[10][10] = 2;
+    array[10][41] = 3;
+    return array
+}
+
+
 function drawBoard() {
-    // let horizontal = BOARD_WIDTH;
-    // let vertical =;
-    //
-    // for(let i = 0; i < )
+    ctx.strokeStyle = "#0f3052"
+    ctx.lineWidth = 1;
+    //Draw horizontal lines
+    for(let i = 0; i < VERTICAL_SQUARES + 1; i++){
+        ctx.beginPath();
+        ctx.moveTo(0, i * a_square);
+        ctx.lineTo(1272, i * a_square);
+        ctx.stroke();
+    }
+
+    //Draw vertical lines
+    for(let i = 0; i < HORIZONTAL_SQUARES + 1; i++){
+        ctx.beginPath();
+        ctx.moveTo(i * a_square, 0);
+        ctx.lineTo(i * a_square, 504);
+        ctx.stroke();
+    }
+
 }
 
 window.addEventListener('load', function (){
