7cd8eff91969b6d7ba68757d4d241b44c339937f
pprzemek56 <przemkopis@wp.pl>
2023-04-06 23:31:28 +0200

    create BoardSerializer

--- webapp/pathfinder/serializers.py
+++ webapp/pathfinder/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
 
+from webapp.pathfinder.models import Board
+
 
 class BoardSerializer(serializers.Serializer):
-    pass
+    board = serializers.ListField(
+        child=serializers.ListField(
+            child=serializers.IntegerField()
+        )
+    )
+    algorithm = serializers.CharField(max_length=200)
+
+    def create(self, validated_data):
+        return Board(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.board = validated_data.get('board', instance.board)
+        instance.algorithm = validated_data.get('algorithm', instance.algorithm)
+        return instance
