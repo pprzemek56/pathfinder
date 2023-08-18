aef3cd0a47696c3e134139adb40399aae581acb7
pprzemek56 <przemkopis@wp.pl>
2023-08-18 21:06:59 +0200

    add navabr, HomePage components

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "../components/navbar/Navbar";
+
+function HomePage(props) {
+    return (
+        <div className='homePage'>
+            <Navbar />
+        </div>
+    );
+}
+
+export default HomePage;
