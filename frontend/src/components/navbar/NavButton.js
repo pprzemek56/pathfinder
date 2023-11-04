0b1a91f5d83c10db3e2550bf3eb55e4c5b185a3f
pprzemek56 <przemkopis@wp.pl>
2023-11-04 02:01:43 +0100

    update buttons label and behavior

--- frontend/src/components/navbar/NavButton.js
+++ frontend/src/components/navbar/NavButton.js
@@ -1,39 +1,36 @@
 import {visualize} from "../../utils/api";
 import {animateShortestPath, animateVisited} from "../board/animations";
-import {useState} from "react";
-
+import "./NavButton.css"
 function NavButton({ id, isRunning, setIsRunning, board, algorithm, canvasRef, start, end }) {
-    const [timeouts, setTimeouts] = useState([]);
     const onButtonClick = async () => {
         const ctx = canvasRef.current.getContext('2d');
 
-        if (!isRunning) {
-            const data = {
-                board: board,
-                algorithm: algorithm,
-            };
+        const data = {
+            board: board,
+            algorithm: algorithm,
+        };
 
+        if (!isRunning) {
             try {
                 const result = await visualize(data);
-                console.log(result);
                 const { visited, shortest_path } = result;
 
                 const totalVisitedAnimationDuration = visited.length * 50;
-                const visitedTimeouts = animateVisited(visited, ctx, start, end);
-
+                const totalShortestPathAnimationDuration = shortest_path.length * 50;
+                setIsRunning(true);
                 animateVisited(visited, ctx, start, end);
 
                 setTimeout(() => {
                     animateShortestPath(shortest_path, ctx, start, end);
+
+                    setTimeout(() => {
+                        setIsRunning(false);
+                    }, totalShortestPathAnimationDuration);
                 }, totalVisitedAnimationDuration);
 
-                setTimeouts([...visitedTimeouts]);
             } catch (error) {
                 console.error("Error visualizing the path:", error.message);
             }
-        } else {
-            timeouts.forEach(timeout => clearTimeout(timeout));
-            console.log("Stopping the visualization algorithm...");
         }
     };
 
@@ -41,11 +38,11 @@ function NavButton({ id, isRunning, setIsRunning, board, algorithm, canvasRef, s
         <li className="nav-item">
             <button 
                 type="button" 
-                className={`btn btn-lg ${isRunning ? "btn-danger" : "btn-primary"}`} 
+                className={`btn btn-lg btn-fixed-width ${isRunning ? "btn-danger" : "btn-primary"}`}
                 id={id}
                 onClick={onButtonClick}
             >
-                {isRunning ? "Stop Visualization" : "Start Visualization"}
+                {isRunning ? "Visualizing..." : "Start Visualization"}
             </button>
         </li>
     );
