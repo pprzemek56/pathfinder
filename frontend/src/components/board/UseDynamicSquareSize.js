434e8914006b7e386b58ac53a44550484583fa95
pprzemek56 <przemkopis@wp.pl>
2023-11-14 21:26:35 +0100

    implement responsiveness of the navbar component

--- frontend/src/components/board/UseDynamicSquareSize.js
+++ frontend/src/components/board/UseDynamicSquareSize.js
@@ -12,7 +12,6 @@ export function UseDynamicSquareSize() {
             const maxWidth = Math.min(containerWidth, window.innerWidth * 0.9); // 90% of the window width
             const size = Math.floor(maxWidth / 53); // Assuming 53 is the number of squares horizontally
 
-            console.log("Calculated square size:", size);
             setSquareSize(Math.min(size, 24)); // Or any maximum size you prefer
         };
 
