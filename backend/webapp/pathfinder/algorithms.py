b8fcb6c8259b165c83d161417ee1205330b90208
pprzemek56 <przemkopis@wp.pl>
2023-11-22 23:52:59 +0100

    implement dijkstras algorithm method

--- backend/webapp/pathfinder/algorithms.py
+++ backend/webapp/pathfinder/algorithms.py
@@ -1,3 +1,4 @@
+import heapq
 from collections import deque
 
 from .models import Board
@@ -5,10 +6,55 @@ from .models import Board
 
 def djikstras_algorithm(board: Board):
     start, end = board.find_start_end()
-    visited = []
-    visited_set = set()
-    stack = [(start, None)]
+
+    # Initialize distances with infinity
+    distances = {(i, j): float('infinity') for i in range(len(board.board)) for j in range(len(board.board[0]))}
+    distances[(start['y'], start['x'])] = 0
+
+    # Priority queue: (distance, node)
+    priority_queue = [(0, start)]
     parents = {}
+    visited = []
+    path_found = False
+
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        cx, cy = current_node['x'], current_node['y']
+
+        # Mark the node as visited
+        if current_node not in visited:
+            visited.append(current_node)
+
+        # Stop if we reach the end node
+        if current_node == end:
+            path_found = True
+            break
+
+        # Check all neighbors
+        for dy, dx in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= ny < len(board.board) and 0 <= nx < len(board.board[0]):
+                neighbor_node = board.board[ny][nx]
+                if neighbor_node['type'] != 1:  # Skip walls
+                    new_distance = current_distance + neighbor_node['weight']
+                    if new_distance < distances[(ny, nx)]:
+                        distances[(ny, nx)] = new_distance
+                        heapq.heappush(priority_queue, (new_distance, {'x': nx, 'y': ny}))
+                        parents[(nx, ny)] = current_node
+
+    if not path_found:
+        return None, None
+
+    # Construct the shortest path
+    shortest_path = []
+    current = end
+    while current != start:
+        shortest_path.append(current)
+        current = parents.get((current['x'], current['y']), start)
+    shortest_path.append(start)
+    shortest_path.reverse()
+
+    return visited, shortest_path
 
 
 def dfs_algorithm(board: Board):
