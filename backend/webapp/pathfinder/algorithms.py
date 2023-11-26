8ca097f007c7a19217ab7aaefae41b238eab96a3
pprzemek56 <przemkopis@wp.pl>
2023-11-26 21:48:15 +0100

    implement a_star_algorithm method

--- backend/webapp/pathfinder/algorithms.py
+++ backend/webapp/pathfinder/algorithms.py
@@ -4,6 +4,67 @@ from collections import deque
 from .models import Board
 
 
+def manhattan_distance(start, end):
+    return abs(start['x'] - end['x']) + abs(start['y'] - end['y'])
+
+
+def a_star_algorithm(board: Board):
+    start, end = board.find_start_end()
+    # Heuristic function - Manhattan distance
+    heuristic = lambda node: manhattan_distance(node, end)
+
+    # Initialize distances with infinity
+    distances = {(i, j): float('infinity') for i in range(len(board.board)) for j in range(len(board.board[0]))}
+    distances[(start['y'], start['x'])] = 0
+
+    # Priority queue: (cost + heuristic, cost, node)
+    priority_queue = [(heuristic(start), 0, start)]
+    parents = {}
+    visited = []
+    path_found = False
+
+    while priority_queue:
+        _, current_cost, current_node = heapq.heappop(priority_queue)
+        cx, cy = current_node['x'], current_node['y']
+
+        # Mark the node as visited
+        if current_node not in visited:
+            visited.append(current_node)
+
+        # Stop if we reach the end node
+        if current_node == end:
+            path_found = True
+            break
+
+        # Check all neighbors
+        for dy, dx in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= ny < len(board.board) and 0 <= nx < len(board.board[0]):
+                neighbor_node = board.board[ny][nx]
+                if neighbor_node['type'] != 1:  # Skip walls
+                    new_cost = current_cost + neighbor_node['weight']
+                    if new_cost < distances[(ny, nx)]:
+                        distances[(ny, nx)] = new_cost
+                        total_cost = new_cost + heuristic({'x': nx, 'y': ny})
+                        heapq.heappush(priority_queue, (total_cost, new_cost, {'x': nx, 'y': ny}))
+                        parents[(nx, ny)] = current_node
+
+    # Check if path found
+    if not path_found:
+        return None, None
+
+    # Construct the shortest path
+    shortest_path = []
+    current = end
+    while current != start:
+        shortest_path.append(current)
+        current = parents.get((current['x'], current['y']), start)
+    shortest_path.append(start)
+    shortest_path.reverse()
+
+    return visited, shortest_path
+
+
 def djikstras_algorithm(board: Board):
     start, end = board.find_start_end()
 
