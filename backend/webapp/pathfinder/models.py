a7f3fa47c90be37cf0a794e4960f810de07db437
pprzemek56 <przemkopis@wp.pl>
2024-01-01 20:58:31 +0100

    reverting commits

--- backend/webapp/pathfinder/models.py
+++ backend/webapp/pathfinder/models.py
@@ -1,76 +1,18 @@
-class Node:
-    def __int__(self, x, y, node_type, neighbors, weight=1):
-        self.x = x
-        self.y = y
-        self.node_type = node_type
-        self.neighbors = neighbors
-        self.weight = weight
-        self.distance = float('infinity')
-        self.parent = None
-
-    @property
-    def x(self):
-        return self._x
-
-    @x.setter
-    def x(self, x):
-        self._x = x
-
-    @property
-    def y(self):
-        return self._y
-
-    @y.setter
-    def y(self, y):
-        self._y = y
-
-    @property
-    def node_type(self):
-        return self._node_type
-
-    @node_type.setter
-    def node_type(self, node_type):
-        self._node_type = node_type
-
-    @property
-    def neighbors(self):
-        return self._neighbors
-
-    @neighbors.setter
-    def neighbors(self, neighbors):
-        self._neighbors = neighbors
-
-    @property
-    def weight(self):
-        return self._weight
-
-    @weight.setter
-    def weight(self, weight):
-        self._weight = weight
-
-    def is_walkable(self):
-        return self.type != 1
-
-    def reset_state(self):
-        self.distance = float('infinity')
-        self.parent = None
-
-
 class Board:
-    def __init__(self, nodes, algorithm):
-        self.nodes = nodes
+    def __init__(self, board: list[list[dict]]):
+        self.board = board
         self.algorithm = algorithm
 
     def __str__(self):
         return f"algorithm: {self.algorithm}"
 
     @property
-    def nodes(self):
-        return self._nodes
+    def board(self):
+        return self._board
 
-    @nodes.setter
-    def nodes(self, nodes):
-        self._nodes = nodes
+    @board.setter
+    def board(self, board):
+        self._board = board
 
     @property
     def algorithm(self):
@@ -81,15 +23,17 @@ class Board:
         self._algorithm = algorithm
 
     def find_start_end(self):
-        start, end = None, None
-        for node in self.nodes:
-            if node.node_type == 2:
-                start = node
-            if node.node_type == 3:
-                end = node
-            if start and end:
-                return start, end
-        return start, end
+        start = None
+        end = None
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j]["type"] == 2:
+                    start = {"x": j, "y": i}
+                elif self.board[i][j]["type"] == 3:
+                    end = {"x": j, "y": i}
+
+                if start and end:
+                    return start, end
 
 
 class Path:
