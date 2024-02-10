b98d3658d013f2ac72e9badc79aef36e84d1ebc5
pprzemek56 <przemkopis@wp.pl>
2024-02-10 14:00:19 +0100

    install channels and channels_redis and implement necessary configuration

--- backend/webapp/webapp/settings.py
+++ backend/webapp/webapp/settings.py
@@ -104,6 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [(os.environ.get('REDIS_HOST', 'localhost'), os.environ.get('REDIS_PORT', 6379))],
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
