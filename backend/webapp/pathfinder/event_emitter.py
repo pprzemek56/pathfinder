f9d75b6e421ebabcdbafc11ffb6cabb5f824ccae
pprzemek56 <przemkopis@wp.pl>
2024-02-20 23:24:47 +0100

    implement debug parameter in emit_event function

--- backend/webapp/pathfinder/event_emitter.py
+++ backend/webapp/pathfinder/event_emitter.py
@@ -4,15 +4,17 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
 
-def emit_event(event_name, message):
-    channel_layer = get_channel_layer()
-    room_group_name = 'group_visualization_room'  # Ensure this matches your consumer group name
+def emit_event(event_name, message, debug=False):
+    if not debug:
+        pass
 
-    # Use async_to_sync to call the asynchronous group_send method
+    channel_layer = get_channel_layer()
+    room_group_name = 'group_visualization_room'
+
     async_to_sync(channel_layer.group_send)(
         room_group_name,
         {
-            'type': 'broadcast_message',  # Matches the method in the consumer
+            'type': 'broadcast_message',
             'message': json.dumps({'event': event_name, 'detail': message})
         }
     )
