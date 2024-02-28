c7dd0fb002f5fd9838a5d112a755879fc1af62b7
pprzemek56 <przemkopis@wp.pl>
2024-02-28 23:00:34 +0100

    implement delay in emiv_event function

--- backend/webapp/pathfinder/event_emitter.py
+++ backend/webapp/pathfinder/event_emitter.py
@@ -1,20 +1,25 @@
+import asyncio
 import json
 
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
 
-def emit_event(event_name, message, debug=False):
+def emit_event(event_name, message, debug):
     if not debug:
-        pass
+        return
 
     channel_layer = get_channel_layer()
     room_group_name = 'group_visualization_room'
 
-    async_to_sync(channel_layer.group_send)(
-        room_group_name,
-        {
-            'type': 'broadcast_message',
-            'message': json.dumps({'event': event_name, 'detail': message})
-        }
-    )
+    async def set_delay():
+        await asyncio.sleep(1)
+        await channel_layer.group_send(
+            room_group_name,
+            {
+                'type': 'broadcast_message',
+                'message': json.dumps({'event': event_name, 'detail': message})
+            }
+        )
+
+    async_to_sync(set_delay)()
