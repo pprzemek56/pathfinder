b055f3b0851f65ac3a67aa68602278d7702e2b2b
pprzemek56 <przemkopis@wp.pl>
2024-02-29 00:04:25 +0100

    add visites and shortest_path value to return from visualize_with_debug function

--- backend/webapp/pathfinder/views.py
+++ backend/webapp/pathfinder/views.py
@@ -48,16 +48,25 @@ def visualize_with_debug(request: Request):
     else:
         return Response(data=board_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if board.algorithm == "dfs":
-        algorithms.dfs_algorithm(board, True)
-    elif board.algorithm == "bfs":
-        algorithms.bfs_algorithm(board, True)
-    elif board.algorithm == "dijkstras":
-        algorithms.djikstras_algorithm(board, True)
-    elif board.algorithm == "a_star":
-        algorithms.a_star_algorithm(board, True)
-    elif board.algorithm == "greedy_bfs":
-        algorithms.greedy_best_first_search(board, True)
+    visited = []
+    shortest_path = []
 
-    return Response({"message": "Debug visualization in progress"}, status=status.HTTP_200_OK)
+    if board.algorithm == "dfs":
+        visited, shortest_path = algorithms.dfs_algorithm(board, True)
+    elif board.algorithm == "bfs":
+        visited, shortest_path = algorithms.bfs_algorithm(board, True)
+    elif board.algorithm == "dijkstras":
+        visited, shortest_path = algorithms.djikstras_algorithm(board, True)
+    elif board.algorithm == "a_star":
+        visited, shortest_path = algorithms.a_star_algorithm(board, True)
+    elif board.algorithm == "greedy_bfs":
+        visited, shortest_path = algorithms.greedy_best_first_search(board, True)
+
+    path_serializer = PathSerializer(data={"visited": visited, "shortest_path": shortest_path})
+    if path_serializer.is_valid():
+        response = Response(data=path_serializer.data)
+    else:
+        return Response(data=path_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return response
 
